2c3939f79b2c3a7f9b8a969b5c4fba5dc5b5910f
quininer kel <quininer@live.com>
2016-06-09 09:33:34 +0800

    rust-crypto bench
    T_T

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,19 +1,36 @@
 #![feature(test)]
 
 extern crate test;
+extern crate crypto;
 extern crate hc128;
 
 use test::Bencher;
-use hc128::HC128;
 
 
 #[bench]
 fn hc128_bench(b: &mut Bencher) {
+    use hc128::HC128;
+
     let key = [0; 16];
     let iv = [0; 16];
     let input = [0; 64];
     let mut output = [0; 64];
     let mut cipher = HC128::new(&key, &iv);
+
+    b.bytes = input.len() as u64;
+    b.iter(|| cipher.process(&input, &mut output))
+}
+
+#[bench]
+fn crypto_bench(b: &mut Bencher) {
+    use crypto::hc128::Hc128;
+    use crypto::symmetriccipher::SynchronousStreamCipher;
+
+    let key = [0; 16];
+    let iv = [0; 16];
+    let input = [0; 64];
+    let mut output = [0; 64];
+    let mut cipher = Hc128::new(&key, &iv);
 
     b.bytes = input.len() as u64;
     b.iter(|| cipher.process(&input, &mut output))
