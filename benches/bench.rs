edca12f8155a024c5d8d427740b4164931f4027d
quininer kel <quininer@live.com>
2017-01-04 23:19:54 +0800

    chore: bench use same key

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,27 +4,37 @@ extern crate test;
 extern crate rand;
 extern crate crypto;
 extern crate hc128;
+#[macro_use] extern crate lazy_static;
 
 use test::Bencher;
 use rand::{ Rng, thread_rng };
+
+lazy_static!{
+    static ref KEY: [u8; 16] = {
+        let mut key = [0; 16];
+        thread_rng().fill_bytes(&mut key);
+        key
+    };
+    static ref IV: [u8; 16] = {
+        let mut iv = [0; 16];
+        thread_rng().fill_bytes(&mut iv);
+        iv
+    };
+    static ref INPUT: [u8; 1024] = {
+        let mut input = [0; 1024];
+        thread_rng().fill_bytes(&mut input);
+        input
+    };
+}
 
 
 #[bench]
 fn hc128_bench_once(b: &mut Bencher) {
     use hc128::HC128;
 
-    let mut rng = thread_rng();
-    let mut key = [0; 16];
-    let mut iv = [0; 16];
-    let mut input = [0; 1024];
     let mut output = [0; 1024];
-    rng.fill_bytes(&mut key);
-    rng.fill_bytes(&mut iv);
-    rng.fill_bytes(&mut input);
-
-
-    b.bytes = input.len() as u64;
-    b.iter(|| HC128::new(&key, &iv).process(&input, &mut output))
+    b.bytes = INPUT.len() as u64;
+    b.iter(|| HC128::new(&*KEY, &*IV).process(&*INPUT, &mut output))
 }
 
 #[bench]
@@ -32,35 +42,19 @@ fn crypto_bench_once(b: &mut Bencher) {
     use crypto::hc128::Hc128;
     use crypto::symmetriccipher::SynchronousStreamCipher;
 
-    let mut rng = thread_rng();
-    let mut key = [0; 16];
-    let mut iv = [0; 16];
-    let mut input = [0; 1024];
     let mut output = [0; 1024];
-    rng.fill_bytes(&mut key);
-    rng.fill_bytes(&mut iv);
-    rng.fill_bytes(&mut input);
-
-    b.bytes = input.len() as u64;
-    b.iter(|| Hc128::new(&key, &iv).process(&input, &mut output))
+    b.bytes = INPUT.len() as u64;
+    b.iter(|| Hc128::new(&*KEY, &*IV).process(&*INPUT, &mut output))
 }
 
 #[bench]
 fn hc128_bench(b: &mut Bencher) {
     use hc128::HC128;
 
-    let mut rng = thread_rng();
-    let mut key = [0; 16];
-    let mut iv = [0; 16];
-    let mut input = [0; 1024];
     let mut output = [0; 1024];
-    let mut cipher = HC128::new(&key, &iv);
-    rng.fill_bytes(&mut key);
-    rng.fill_bytes(&mut iv);
-    rng.fill_bytes(&mut input);
-
-    b.bytes = input.len() as u64;
-    b.iter(|| cipher.process(&input, &mut output))
+    let mut cipher = HC128::new(&*KEY, &*IV);
+    b.bytes = INPUT.len() as u64;
+    b.iter(|| cipher.process(&*INPUT, &mut output))
 }
 
 #[bench]
@@ -68,16 +62,8 @@ fn crypto_bench(b: &mut Bencher) {
     use crypto::hc128::Hc128;
     use crypto::symmetriccipher::SynchronousStreamCipher;
 
-    let mut rng = thread_rng();
-    let mut key = [0; 16];
-    let mut iv = [0; 16];
-    let mut input = [0; 1024];
     let mut output = [0; 1024];
-    let mut cipher = Hc128::new(&key, &iv);
-    rng.fill_bytes(&mut key);
-    rng.fill_bytes(&mut iv);
-    rng.fill_bytes(&mut input);
-
-    b.bytes = input.len() as u64;
-    b.iter(|| cipher.process(&input, &mut output))
+    let mut cipher = Hc128::new(&*KEY, &*IV);
+    b.bytes = INPUT.len() as u64;
+    b.iter(|| cipher.process(&*INPUT, &mut output))
 }
